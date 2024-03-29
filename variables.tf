3f4d53f2db6e85ef9dbd452bb172a60507d239d3
Taha <tahabozoyuk@gmail.com>
2024-03-29 16:55:59 +0300

    first

---
+++ variables.tf
@@ -0,0 +1,16 @@
+
+variable "git-name" {
+  default = "tahabozoyuk"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "esra"
+}
+
+variable "hosted-zone" {
+  default = "tahabozoyukaws.click"
+}
